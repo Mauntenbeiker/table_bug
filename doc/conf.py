cdf947a5c4c0a6922838105a4b1195b59549ab11
Kevin Förster <kevin.foerster.ext@siemens.com>
2023-10-17 11:59:22 +0200

    Created a new conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -1,5 +1,28 @@
-import os
-import sys
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Options for PDF output ----------------------
-latex_engine = 'xelatex'
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'table_bug'
+copyright = '2023, Mauntenbeiker'
+author = 'Mauntenbeiker'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
