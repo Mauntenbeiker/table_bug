a3833019c50284ba41f133417068b87440162f33
Kevin Förster <kevin.foerster.ext@siemens.com>
2023-10-17 10:48:38 +0200

    Created a minimal method to reproduce the issue.

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("./_static"))
+#sys.path.insert(0, os.path.abspath("./_static"))
 
 # -- Options for PDF output ----------------------
 latex_engine = 'xelatex'
